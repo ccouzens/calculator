ac5303e5eb96608c8d1bfd0e109ff3c4ec0d31a8
Chris Couzens <ccouzens@gmail.com>
2013-09-29 21:27:21 +0100

    Added tests for lexing

--- parse.go
+++ parse.go
@@ -1,7 +1,32 @@
 package calculator
 
-import "strconv"
+import (
+	"regexp"
+	"strconv"
+)
+
+type token_type int
+
+var white_space_regex = regexp.MustCompile("^\\s+")
+var literal_number_regex = regexp.MustCompile("^\\d+")
+var plus_regex = regexp.MustCompile("^\\+")
+
+const (
+	t_white_space token_type = iota
+	t_literal_number
+	t_plus
+)
+
+type Token struct {
+	Lexeme string
+	Type   token_type
+}
+
+func Lex(input string) ([]Token, error) {
+	return []Token{}, nil
+}
 
 func Parse(input string) (int64, error) {
+
 	return strconv.ParseInt(input, 10, 64)
 }
--- parse_test.go
+++ parse_test.go
@@ -2,7 +2,75 @@ package calculator
 
 import "testing"
 
-func TestLiteral(t *testing.T) {
+func TestLexLiteral(t *testing.T) {
+	const in = "45"
+	var out1 = Token{in, t_literal_number}
+	const length = 1
+	x, err := Lex(in)
+	if err != nil {
+		t.Errorf("Lex(%v) raised %v", in, err)
+	} else if len(x) != length {
+		t.Errorf("Lex(%v) returned %d tokens, want %d", in, len(x), length)
+	} else if x[0] != out1 {
+		t.Errorf("Lex(%v) = %v, want %v", in, x, out1)
+	}
+}
+
+func TestLexWhiteSpace(t *testing.T) {
+	const in = " \t"
+	var out1 = Token{in, t_white_space}
+	const length = 1
+	x, err := Lex(in)
+	if err != nil {
+		t.Errorf("Lex(%v) raised %v", in, err)
+	} else if len(x) != length {
+		t.Errorf("Lex(%v) returned %d tokens, want %d", in, len(x), length)
+	} else if x[0] != out1 {
+		t.Errorf("Lex(%v) = %v, want %v", in, x, out1)
+	}
+}
+
+func TestLexPlus(t *testing.T) {
+	const in = "+"
+	var out1 = Token{in, t_plus}
+	const length = 1
+	x, err := Lex(in)
+	if err != nil {
+		t.Errorf("Lex(%v) raised %v", in, err)
+	} else if len(x) != length {
+		t.Errorf("Lex(%v) returned %d tokens, want %d", in, len(x), length)
+	} else if x[0] != out1 {
+		t.Errorf("Lex(%v) = %v, want %v", in, x, out1)
+	}
+}
+
+func TestLexCompound(t *testing.T) {
+	const in = "3 + 28"
+	var out = []Token{
+		{"3", t_literal_number},
+		{" ", t_white_space},
+		{"+", t_plus},
+		{" ", t_white_space},
+		{"28", t_literal_number},
+	}
+
+	const length = 5
+	x, err := Lex(in)
+	if err != nil {
+		t.Errorf("Lex(%v) raised %v", in, err)
+	} else if len(x) != length {
+		t.Errorf("Lex(%v) returned %d tokens, want %d", in, len(x), length)
+	} else {
+		for i := 0; i < length; i++ {
+			if x[i] != out[i] {
+				t.Errorf("Lex(%v)[%d] = %v, want %v", in, i, x[i], out[i])
+				return
+			}
+		}
+	}
+}
+
+func TestParseLiteral(t *testing.T) {
 	const in, out = "45", 45
 	x, err := Parse(in)
 	if err != nil {
@@ -12,7 +80,7 @@ func TestLiteral(t *testing.T) {
 	}
 }
 
-func TestAddition(t *testing.T) {
+func TestParseAddition(t *testing.T) {
 	const in, out = "3 + 5", 8
 	x, err := Parse(in)
 	if err != nil {
